63293064bb9e0928acf84dfbbb6f463d94268e14
Eliphaz <eliahlander3@gmail.com>
2022-04-11 15:27:56 -0600

    added average collumn

--- sfbikes.py
+++ sfbikes.py
@@ -24,5 +24,10 @@ def least_bikes_recorded(df, year):
     return (f'the least bikes recorded at one location in {year} is {min(min_arr)}')
 
 
+cols = ['YR2006_PM', 'YR2007_PM', 'YR2008_PM', 'YR2009_PM', 'YR2010_PM',
+        'YR2011_PM', 'YR2013_PM', 'YR2014_PM', 'YR2015_PM', 'YR2016_PM', 'YR2017_PM', ]
+df['average'] = df[cols].astype(float).mean(axis=1)
+print(df['average'])
+
 print(least_bikes_recorded(df, 2011))
 #print(most_bikes_recorded(df, 2016))
